ca79324f1437a63152b43126050bba75b358ac83
tbytes404 <tbytes404@icloud.com>
2025-05-18 01:39:24 +0530

    ready to share

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-
 import { GestureRecognizer, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.js"
 
-// Create task for image file processing:
-const vision = await FilesetResolver.forVisionTasks(
-  // path/to/wasm/root
-  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm"
-);
+const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm");
 const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
-  baseOptions: {
-    modelAssetPath: "./exported_model/gesture_recognizer.task"
-    // "https://storage.googleapis.com/mediapipe-tasks/gesture_recognizer/gesture_recognizer.task"
-  },
+  baseOptions: { modelAssetPath: "./exported_model/gesture_recognizer.task" },
   numHands: 2
 });
 
-const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const videoElement = document.getElementById('input_video');
+const canvasElement = document.getElementById('output_canvas');
+const gestureText = document.getElementById('gesture_text');
 const canvasCtx = canvasElement.getContext('2d');
-const tagText = document.createElement("p")
-document.body.appendChild(tagText);
 
 async function onResults(results) {
   const res = await gestureRecognizer.recognize(results.image);
   if (res.gestures.length > 0)
-    tagText.textContent = res.gestures[0][0].categoryName;
+    gestureText.textContent = res.gestures[0][0].categoryName;
 
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-    results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiHandLandmarks) {
+  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  if (results.multiHandLandmarks)
     for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-        { color: '#00FF00', lineWidth: 5 });
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 5 });
       drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
     }
-  }
   canvasCtx.restore();
 }
 
@@ -60,4 +49,3 @@ const camera = new Camera(videoElement, {
   height: 720
 });
 camera.start();
-
--- train.py
+++ train.py
@@ -16,27 +16,7 @@ options = gesture_recognizer.GestureRecognizerOptions(hparams=hparams)
 model = gesture_recognizer.GestureRecognizer.create(
     train_data=train_data, validation_data=validation_data, options=options
 )
+
 loss, acc = model.evaluate(test_data, batch_size=1)
 print(f"Test loss:{loss}, Test accuracy:{acc}")
 model.export_model()
-
-# import os
-# import matplotlib.pyplot as plt
-#
-# labels = []
-# for i in os.listdir(dataset_path):
-#     if os.path.isdir(os.path.join(dataset_path, i)):
-#         labels.append(i)
-# print(labels)
-
-# NUM_EXAMPLES = 5
-# for label in labels:
-#     label_dir = os.path.join(dataset_path, label)
-#     example_filenames = os.listdir(label_dir)[:NUM_EXAMPLES]
-#     fig, axs = plt.subplots(1, NUM_EXAMPLES, figsize=(10, 2))
-#     for i in range(NUM_EXAMPLES):
-#         axs[i].imshow(plt.imread(os.path.join(label_dir, example_filenames[i])))
-#         axs[i].get_xaxis().set_visible(False)
-#         axs[i].get_yaxis().set_visible(False)
-#     fig.suptitle(f"Showing {NUM_EXAMPLES} examples for {label}")
-# plt.show()
