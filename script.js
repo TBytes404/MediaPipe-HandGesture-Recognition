18e9f85b733e824b4bbe025a9fee7be8d7aa0f35
tbytes404 <tbytes404@icloud.com>
2025-05-19 21:51:44 +0530

    using video mode for recognition

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-import "https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js"
-import "https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js"
-import "https://cdn.jsdelivr.net/npm/@mediapipe/hands/hands.js"
-import { GestureRecognizer, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.js"
+import { GestureRecognizer, FilesetResolver, DrawingUtils } from
+  "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/vision_bundle.js"
 
-const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm");
+const video = document.getElementById('video');
+const gesture = document.getElementById('gesture');
+const canvas = document.getElementById('canvas');
+
+const ctx = canvas.getContext('2d');
+const drawing = new DrawingUtils(ctx);
+
+const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
 const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
-  baseOptions: { modelAssetPath: "./exported_model/gesture_recognizer.task" },
+  baseOptions: {
+    modelAssetPath: "./exported_model/gesture_recognizer.task",
+    delegate: "GPU"
+  },
+  runningMode: "VIDEO"
 });
 
-const videoElement = document.getElementById('input_video');
-const canvasElement = document.getElementById('output_canvas');
-const gestureText = document.getElementById('gesture_text');
-const canvasCtx = canvasElement.getContext('2d');
-
-async function onFrame() {
-  const results = await gestureRecognizer.recognize(videoElement)
+let lastFrameTime = 0;
+function onFrame() {
+  const results = gestureRecognizer.recognizeForVideo(video, lastFrameTime);
   if (results.gestures.length > 0)
-    gestureText.textContent = results.gestures[0][0].categoryName || 'none';
+    gesture.textContent = 'Gesture: ' + (results.gestures[0][0].categoryName || 'none');
 
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
-
+  ctx.save();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   if (results.landmarks)
     for (const landmarks of results.landmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#31748f', lineWidth: 2 });
-      drawLandmarks(canvasCtx, landmarks, { color: '#eb6f92', lineWidth: 2 });
+      drawing.drawConnectors(landmarks, GestureRecognizer.HAND_CONNECTIONS, { color: '#31748f', lineWidth: 5 });
+      drawing.drawLandmarks(landmarks, { color: '#eb6f92', lineWidth: 2 });
     }
-  canvasCtx.restore();
+  ctx.restore();
+
+  lastFrameTime += 1;
+  requestAnimationFrame(onFrame);
 }
 
-const camera = new Camera(videoElement, {
-  onFrame, width: 640, height: 360
+const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+video.srcObject = stream;
+video.addEventListener("loadeddata", () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  requestAnimationFrame(onFrame);
 });
-camera.start();
