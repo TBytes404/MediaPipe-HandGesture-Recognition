0875d5e048f9bc40388061f39f0aa3ca79f5fa18
tbytes404 <tbytes404@icloud.com>
2025-05-19 14:56:07 +0530

    delete redundant landmark detection

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+import "https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js"
+import "https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js"
+import "https://cdn.jsdelivr.net/npm/@mediapipe/hands/hands.js"
 import { GestureRecognizer, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.js"
 
 const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm");
 const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
   baseOptions: { modelAssetPath: "./exported_model/gesture_recognizer.task" },
-  numHands: 2
 });
 
 const videoElement = document.getElementById('input_video');
@@ -11,41 +13,24 @@ const canvasElement = document.getElementById('output_canvas');
 const gestureText = document.getElementById('gesture_text');
 const canvasCtx = canvasElement.getContext('2d');
 
-async function onResults(results) {
-  const res = await gestureRecognizer.recognize(results.image);
-  if (res.gestures.length > 0)
-    gestureText.textContent = res.gestures[0][0].categoryName;
+async function onFrame() {
+  const results = await gestureRecognizer.recognize(videoElement)
+  if (results.gestures.length > 0)
+    gestureText.textContent = results.gestures[0][0].categoryName || 'none';
 
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
 
-  if (results.multiHandLandmarks)
-    for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#00FF00', lineWidth: 5 });
-      drawLandmarks(canvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+  if (results.landmarks)
+    for (const landmarks of results.landmarks) {
+      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#31748f', lineWidth: 2 });
+      drawLandmarks(canvasCtx, landmarks, { color: '#eb6f92', lineWidth: 2 });
     }
   canvasCtx.restore();
 }
 
-const hands = new Hands({
-  locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-  }
-});
-hands.setOptions({
-  maxNumHands: 2,
-  modelComplexity: 1,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
-});
-hands.onResults(onResults);
-
 const camera = new Camera(videoElement, {
-  onFrame: async () => {
-    await hands.send({ image: videoElement });
-  },
-  width: 1280,
-  height: 720
+  onFrame, width: 640, height: 360
 });
 camera.start();
