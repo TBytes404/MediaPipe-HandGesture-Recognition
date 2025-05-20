1bafb767ede8514effc806537ecd96c7cd3eb5fc
tbytes404 <tbytes404@icloud.com>
2025-05-20 16:36:08 +0530

    gesture selection stabilization

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 import { GestureRecognizer, FilesetResolver, DrawingUtils } from
   "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/vision_bundle.js"
 
+const gestures = document.querySelectorAll('.gestures>li');
 const video = document.getElementById('video');
-const gesture = document.getElementById('gesture');
 const canvas = document.getElementById('canvas');
+
+const gestureNames = ['rock', 'paper', 'scissors'];
+let stableGestureIdx = Math.floor(Math.random() * gestureNames.length);
+gestures[stableGestureIdx].classList.add('active');
+
+function updateSelection(gesture) {
+  let idx = gestureNames.indexOf(gesture);
+  if (idx === -1 || idx === stableGestureIdx) return;
+  gestures[idx].classList.add('active');
+  gestures[stableGestureIdx].classList.remove('active');
+  stableGestureIdx = idx;
+}
 
 const ctx = canvas.getContext('2d');
 const drawing = new DrawingUtils(ctx);
-
 const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
 const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
   baseOptions: {
     modelAssetPath: "./exported_model/gesture_recognizer.task",
     delegate: "GPU"
-  },
-  runningMode: "VIDEO"
+  }, runningMode: "VIDEO"
 });
 
 let lastFrameTime = 0;
 function onFrame() {
   const results = gestureRecognizer.recognizeForVideo(video, lastFrameTime);
   if (results.gestures.length > 0)
-    gesture.textContent = 'Gesture: ' + (results.gestures[0][0].categoryName || 'none');
+    updateSelection(results.gestures[0][0].categoryName);
 
   ctx.save();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
